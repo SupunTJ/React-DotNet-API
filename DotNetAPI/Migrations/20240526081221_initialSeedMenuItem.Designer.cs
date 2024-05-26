1a3b14f53f5e38523471d2cf59f0d4c81d61fe23
SupunTJ <jayaweeraspn@gmail.com>
2024-05-26 15:00:27 +0530

    Creating Menu Item Controller

--- DotNetAPI/Migrations/20240526081221_initialSeedMenuItem.Designer.cs
+++ DotNetAPI/Migrations/20240526081221_initialSeedMenuItem.Designer.cs
@@ -4,6 +4,7 @@ using DotNetAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotNetAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240526081221_initialSeedMenuItem")]
+    partial class initialSeedMenuItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
